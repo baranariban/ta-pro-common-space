53fdb6dc25e273e2e3e09b29b9a10ea329c2b535
baranariban <alibaranariban.metu@gmail.com>
2025-07-22 13:50:42 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,3 +1,15 @@
 # auth.py
+
+# Kullanıcı adı - şifre sözlüğü
+USER_CREDENTIALS = {
+    "baranariban": "smart",
+    "alioguz": "smart",
+    "neciphayran": "smart",
+    "sertacaltinok": "smart",
+    "zeynepegeuysal": "smart",
+    "ahmetcangunaydin": "smart",
+    "busraunlu": "smart"
+}
+
 def check_credentials(username, password):
-    return username == "baranariban" and password == "smart"
+    return USER_CREDENTIALS.get(username) == password
