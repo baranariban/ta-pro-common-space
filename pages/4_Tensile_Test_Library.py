1cd87af4013afcb36f5cb688c443b28dcd454bb9
baranariban <alibaranariban.metu@gmail.com>
2025-08-27 12:27:26 +0300

    Update 4_Tensile_Test_Library.py

--- pages/4_Tensile_Test_Library.py
+++ pages/4_Tensile_Test_Library.py
@@ -270,6 +270,8 @@ for name in selected_names:
 
         # Ortak grafiğe ekle
         combined_ax.plot(df_result["Strain (%)"], df_result["Stress (MPa)"], label=name)
+        combined_ax.set_xlim(0, 5)
+        combined_ax.set_ylim(0, 80)
 
     except Exception as e:
         st.error(f"❌ Error in file '{file_info['original_filename']}': {e}")
